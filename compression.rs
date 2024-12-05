b25ab3d4bd393904c28e2ee80058f70ce6529307
0x0mini <163350822+0x0mini@users.noreply.github.com>
2024-12-05 14:19:06 +0100

    optimize the naming

--- compression.rs
+++ compression.rs
@@ -6,33 +6,33 @@ use std::io::{self, BufReader, BufWriter, Read, Write};
 mod rusty_compressor {
     use super::*;
 
-    pub fn compress_file(input_path: &str, output_path: &str) -> io::Result<()> {
-        let input_file = File::open(input_path)?;
-        let buffered_reader = BufReader::new(input_file);
+    pub fn compress_to_gzip(input_file_path: &str, compressed_output_path: &str) -> io::Result<()> {
+        let input_file = File::open(input_file_path)?;
+        let input_file_buffered_reader = BufReader::new(input_file);
 
-        let output_file = File::create(output_path)?;
-        let buffered_writer = BufWriter::new(output_file);
-        let mut gz_encoder = GzEncoder::new(buffered_writer, Compression::default());
+        let compressed_output_file = File::create(compressed_output_path)?;
+        let compressed_output_writer = BufWriter::new(compressed_output_file);
+        let mut gzip_encoder = GzEncoder::new(compressed_output_writer, Compression::default());
 
-        let mut buffer = Vec::new();
-        buffered_reader.take(10 * 1024 * 1024).read_to_end(&mut buffer)?;
-        gz_encoder.write_all(&buffer)?;
-        gz_encoder.finish()?;
+        let mut read_buffer = Vec::new();
+        input_file_buffered_reader.take(10 * 1024 * 1024).read_to_end(&mut read_buffer)?;
+        gzip_encoder.write_all(&read_buffer)?;
+        gzip_encoder.finish()?;
 
         Ok(())
     }
 
-    pub fn decompress_file(input_path: &str, output_path: &str) -> io::Result<()> {
-        let input_file = File::open(input_path)?;
-        let buffered_reader = BufReader::new(input_file);
-        let mut gz_decoder = GzDecoder::new(buffered_reader);
+    pub fn decompress_from_gzip(gzip_input_path: &str, decompressed_output_path: &str) -> io::Result<()> {
+        let gzip_input_file = File::open(gzip_input_path)?;
+        let gzip_input_reader = BufReader::new(gzip_input_file);
+        let mut gzip_decoder = GzDecoder::new(gzip_input_reader);
 
-        let output_file = File::create(output_path)?;
-        let mut buffered_writer = BufWriter::new(output_file);
+        let decompressed_output_file = File::create(decompressed_output_path)?;
+        let decompressed_output_writer = BufWriter::new(decompressed_output_file);
 
-        let mut buffer = Vec::new();
-        gz_decoder.read_to_end(&mut buffer)?;
-        buffered_writer.write_all(&buffer)?;
+        let mut decompression_buffer = Vec::new();
+        gzip_decoder.read_to_end(&mut decompression_buffer)?;
+        decompressed_output_writer.write_all(&decompression_buffer)?;
 
         Ok(())
     }
